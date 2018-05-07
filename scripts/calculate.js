4e2c265167f8c367b20d999b7095ae7cee6dd277
dandersonstack <Daniel.anderson@tapingo.com>
2018-05-07 13:43:34 +0300

    initial commit, problem complete, no test support

---
+++ scripts/calculate.js
@@ -0,0 +1,58 @@
+const fs = require('file-system');
+const {
+    parseCSVFile,
+    validateAndGenerateSelectedMoments,
+    calculateExpectedRevenue} = require('./calculator_helpers');
+
+class Calculator {
+    constructor() {
+        this.pathToFile = null;
+        this.selected_month_string = null;
+        this.parsedCSVFile = null;
+    }
+
+    grabInputsFromArgs() {
+        let args = process.argv.slice(2);
+        if(args.length <= 1) {
+            throw new Error('Missing Arguments, please provide an input file followed by a Year-Month object (YYYY-MM).')
+        }
+        this.pathToFile = args[0];
+        this.selected_month_string = args[1];
+    }
+
+    basicValidateInputArguments() {
+        this.validateFileExists();
+        this.validateInputDate();
+    }
+
+    validateInputDate() {
+        if(!this.selected_month_string.match(/^\d{4}-\d{2}$/g)) {
+            throw new Error('The Year-Month object does follow the correct syntax (YYYY-MM).')
+        }
+    }
+
+    validateFileExists() {
+        if(!fs.existsSync(this.pathToFile)) {
+            throw new Error('The file does not exist with the provided path.')
+        }
+    }
+
+    async mainFunction() {
+        try {
+            this.grabInputsFromArgs();
+            this.basicValidateInputArguments();
+            let selected_month_values = validateAndGenerateSelectedMoments(this.selected_month_string);
+            let parsedCSVFile = await (parseCSVFile(this.pathToFile));
+            let calculatedValues = calculateExpectedRevenue(parsedCSVFile, selected_month_values);
+            console.log(calculatedValues)
+            // readOut(this.selected_month_string, calculatedValues.expected_revenue, calculatedValues.unresolved_office_capacity);
+        } catch(error) {
+            console.log(error);
+        }
+
+    }
+
+}
+
+let CalculateProfitsBot = new Calculator();
+CalculateProfitsBot.mainFunction();
---
+++ scripts/calculator_helpers.js
@@ -0,0 +1,83 @@
+const csv = require('csvtojson');
+const moment = require('moment');
+
+const parseCSVFile = async function(path_to_data_file) {
+    return (await new Promise((resolve, reject) => {
+      const promises = [];
+      csv()
+        .fromFile(path_to_data_file)
+        .on('json', converted => promises.push(Promise.resolve(converted)))
+        .on('done', error => {
+          if (error) {
+            reject(error);
+            return
+          }
+        Promise.all(promises).then(convertedResults => resolve(convertedResults))
+      })
+    }).then((parsedCSVFile)=>{
+        return parsedCSVFile;
+    }).catch((err)=>{
+        throw new Error('There was an error parsing the csv file.')
+    }) )
+};
+
+const validateAndGenerateSelectedMoments = function(selected_month_string) {
+        if(!selected_month_string.match(/^\d{4}-\d{2}$/g)) {
+            throw new Error('The Year-Month object does follow the correct syntax (YYYY-MM).')
+        }
+        let selected_month_moment = moment(selected_month_string + '-01');
+        if(!selected_month_moment.isValid()) {
+            throw new Error('The Year-Month object you provide is invalid.')
+        }
+        return {selected_month_start_date: selected_month_moment,
+                selected_month_end_date: moment(selected_month_moment).endOf('months')}
+};
+
+const calculateExpectedRevenue = function (csv_data_in_json, selected_month_dates) {
+
+    let expected_revenue = 0;
+    let unresolved_office_capacity = 0;
+    // expected reservation: { Capacity: '14', 'Monthly Price': '11875', 'Start Day': '2014-06-01', 'End Day': '' },
+    csv_data_in_json.forEach((reservation)=>{
+        let rental_start_date = moment(reservation['Start Day']);
+        let rental_end_date = reservation['End Day'] === ''? moment() : moment(reservation['End Day']);
+        let selected_month_start_date = selected_month_dates.selected_month_start_date;
+        let selected_month_end_date = selected_month_dates.selected_month_end_date;
+
+        if(rental_start_date.isAfter(selected_month_end_date) ||
+            rental_end_date.isBefore(selected_month_start_date)) {
+            unresolved_office_capacity += Number.parseInt(reservation.Capacity);
+        } else if (rental_start_date.isBefore(selected_month_start_date) &&
+                    rental_end_date.isAfter(selected_month_end_date)) {
+            expected_revenue += Number.parseInt(reservation['Monthly Price'])
+        } else {
+           expected_revenue += calculateProRate(
+               reservation['Monthly Price'],
+               rental_start_date,
+               rental_end_date,
+               selected_month_end_date,
+               selected_month_start_date)
+        }
+    });
+    return {expected_revenue: expected_revenue, unresolved_office_capacity: unresolved_office_capacity}
+};
+
+const calculateProRate = function (monthly_price, rental_start_date, rental_end_date, selected_month_end_date, selected_month_start_date) {
+    let days_in_month = rental_start_date.daysInMonth();
+    if(selected_month_start_date.isBefore(rental_start_date)
+       && selected_month_end_date.isAfter(rental_end_date)) {
+        return Math.ceil((rental_end_date.diff(rental_start_date, 'days') / days_in_month ) * monthly_price)
+    } else if( selected_month_start_date.isAfter(rental_start_date) ) { //only the calculate the end of the month
+        console.log('it should calculate the beginning');
+        return Math.ceil((rental_end_date.diff(selected_month_start_date, 'days') / days_in_month ) * monthly_price)
+    } else { //only calculate the beginning of the month
+        console.log('the months are between');
+        return Math.ceil((selected_month_end_date.diff(rental_start_date, 'days')  / days_in_month ) * monthly_price)
+    }
+};
+
+module.exports = {
+    parseCSVFile,
+    validateAndGenerateSelectedMoments,
+    calculateExpectedRevenue
+}
